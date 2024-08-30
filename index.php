2630342c828a89367fc30d434e70ff4835c5b1fa
Sainan <sainan@calamity.inc>
2024-08-30 12:50:29 +0200

    One less redirect

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Refresh: 0; url=https://github.com/agnostic-pm/use.agnostic.pm#readme");
+header("Refresh: 0; url=https://github.com/PlutoLang/use.agnostic.pm#readme");
 header("Content-Type: text/pluto-lang.org");
 ?>
 package.preload["gitwit"] = function()
